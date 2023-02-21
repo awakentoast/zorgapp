fd98b947402c3693c0d10620ea748d190ac03803
Luuk Bouma <luuk.bouma@student.hu.nl>
2023-02-22 00:20:17 +0100

    nice2

--- src/PrintToScreen.java
+++ src/PrintToScreen.java
@@ -15,7 +15,6 @@ public class PrintToScreen {
         System.out.format("%d:  Add a patient\\patients\n", 6);
         System.out.format("%d:  Remove a patient\n\n", 7);
         System.out.print("enter #choice: ");
-        System.out.println("gitgitgitgit");
     }
 
     public static void printSwapPatient() {
